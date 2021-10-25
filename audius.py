2fb0da1150380258b2d056fbdcfe59acb88dfc3d
MaleVTuber <contact.notashe@gmail.com>
2021-10-25 14:34:12 -0400

    finalized. this works now btw

--- audius.py
+++ audius.py
@@ -5,17 +5,18 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
 class hookBase:
-    def __init__(self, username, hookurl, interval=5):
+    def __init__(self, username, hookurl, ignoreOld=None, interval=5):
         self.host = "https://discovery-b.mainnet.audius.radar.tech/v1"
         self.username = username
         self.id = self.getId()
         self.hookurl = hookurl
         self.interval = interval # Interval is set in minutes. Is the duration between checks (duh) for new tracks
         self.curTracks = 0 # Set persistent variable to check if a new track exists
+        self.ignoreOld = bool(ignoreOld) # If true, ignores old tracks
+        self.hasnotignored = True
     
     def sendHook(self, dataIn):
-        webhook = DiscordWebhook(url=self.hookurl, username="Audius Music")
-
+        webhook = DiscordWebhook(url=self.hookurl, username="Audius Music", rate_limit_retry=True)
         embed = DiscordEmbed(
             title=dataIn['title'], description=f"New music posted by {self.username}", color='03b2f8'
         )
@@ -46,42 +47,55 @@ class hookBase:
                 return item['id']
     
     def getTracks(self):
-        headers = {
-        'Accept': 'application/json'
-        }
-        payload = { 
-            'user_id': self.id,
-            'app_name': 'amullerWebhook',
-        }
-        r = requests.get(self.host+f"/users/{self.id}/tracks", params=payload, headers=headers)
-        curLen = len(r.json()['data'])
-        if curLen > self.curTracks:
-            for i in range(curLen-self.curTracks):
-                curData = r.json()['data'][i]
-                data = {
-                    "cover": curData['artwork']['1000x1000'],
-                    "title": curData['title'],
-                    "url": f"audius.co{curData['permalink']}"
+        notRateLimited = False
+        while notRateLimited == False:
+            try:
+                headers = {
+                'Accept': 'application/json'
                 }
-                try:
-                    self.sendHook(data)
-                except Exception as e:
-                    print(f"Error: {e}")
-                time.sleep(0.25) # Avoid rate limiting by Discord CDN
-        self.curTracks = curLen
+                payload = { 
+                    'user_id': self.id,
+                    'app_name': 'amullerWebhook',
+                }
+                r = requests.get(self.host+f"/users/{self.id}/tracks", params=payload, headers=headers)
+                if self.ignoreOld == True and self.hasnotignored == True:
+                    self.curTracks = len(r.json()['data'])
+                    self.hasnotignored = False
+                curLen = len(r.json()['data'])
+                if curLen > self.curTracks:
+                    for i in range(curLen-self.curTracks):
+                        curData = r.json()['data'][i]
+                        data = {
+                            "cover": curData['artwork']['1000x1000'],
+                            "title": curData['title'],
+                            "url": f"audius.co{curData['permalink']}"
+                        }
+                        try:
+                            self.sendHook(data)
+                        except Exception as e:
+                            print(f"Error: {e}")
+                        time.sleep(0.25) # Avoid rate limiting by Discord CDN
+                self.curTracks = curLen
+                notRateLimited = True
+            except Exception as e:
+                print(f"Error: {e}")
+                time.sleep(1)
 
     def run(self):
         while True:
             self.getTracks()
             time.sleep(self.interval)
+
 run = True
 match len(sys.argv):
     case 3:
         h = hookBase(sys.argv[1], sys.argv[2])
     case 4:
         h = hookBase(sys.argv[1], sys.argv[2], sys.argv[3])
+    case 5:
+        h = hookBase(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     case default:
         run = False
-        print("Not enough args given. Example usage: python audius.py <audius handle> <webhook url> <optional: hook check interval (seconds)>")
+        print("Not enough args given. Example usage: python audius.py <audius handle> <webhook url> <optional: ignore latest posts and only fetch new after hook starts (bool)> <optional: hook check interval (seconds)>")
 if run:
     h.run()
